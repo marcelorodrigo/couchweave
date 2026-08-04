2e8b4a64a5079a1f4790360ba5e9b02582768a6e
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-08-04 20:07:06 +0200

    build: Configure centralized Spotless formatting
    Add Spotless 3.9.0 to the root Maven build with CleanThat 2.25 and Palantir Java Format 2.96.0 so formatting is enforced across the reactor. Normalize existing Java sources to satisfy the new check.
    
    Fixes #43

--- samples/couchweave-sample/src/test/java/io/github/marcelorodrigo/couchweave/sample/ReactorBuildIT.java
+++ samples/couchweave-sample/src/test/java/io/github/marcelorodrigo/couchweave/sample/ReactorBuildIT.java
@@ -1,9 +1,9 @@
 package io.github.marcelorodrigo.couchweave.sample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ReactorBuildIT {
 
